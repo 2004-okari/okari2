de285bcffc93eb9c01cb313a5b2cf4529d906056
okari <nyandika15@gmail.com>
2024-03-21 17:30:24 +0300

    Update Introduction.jsx

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Introduction = () => {
+  return (
+    <div>
+      <div>
+        <h2>I'm Okari Nyandika. Glad to see you!</h2>
+        <p>
+          I'm a software developer. I can help you build a mobile app, website
+          or product. Look through some of my work and experience. If you like
+          what you see and have a project you need coded, don't hesitate to
+          contact me.
+        </p>
+        <div>
+          <p>LET'S CONNECT</p>
+          <ul>
+            <li>
+              <a href="/"></a>
+            </li>
+            <li>
+              <a href="/"></a>
+            </li>
+            <li>
+              <a href="/"></a>
+            </li>
+            <li>
+              <a href="/"></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
