2b3bb799b21f18b439fc9aacbd0f791a9daad2d5
okari <nyandika15@gmail.com>
2024-03-21 11:41:04 +0300

    Create Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <p style={{ fontSize: '50px', color: 'yellowgreen'}}>Okari Nyandika</p>
+    </div>
+  );
+};
+
+export default Home;
