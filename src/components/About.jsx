57b7d5a25033f30d61eb7bb2784952707757b8a5
okari <nyandika15@gmail.com>
2024-03-21 17:30:18 +0300

    Update About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <div>
+        <h3>About Myself</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus,
+          ratione. Autem sunt, ipsam voluptates totam quis, asperiores similique
+          quisquam repudiandae aliquid officia alias voluptate minus nostrum?
+          Aliquam quod repudiandae omnis.
+        </p>
+        <div>
+          <p>LET'S CONNECT</p>
+          <ul>
+            <li>
+              <a href="/"></a>
+            </li>
+            <li>
+              <a href="/"></a>
+            </li>
+            <li>
+              <a href="/"></a>
+            </li>
+            <li>
+              <a href="/"></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <p>Languages</p>
+        <p>Frameworks</p>
+        <p>Skills</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
