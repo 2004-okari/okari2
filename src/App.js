f82aaea91cc067d766e0358b6fff65c5e4eb4a96
okari <nyandika15@gmail.com>
2024-03-21 17:30:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
+// import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" component={Home} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
+          <Route path="/" element={<Navigation />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
@@ -19,3 +20,4 @@ function App() {
 }
 
 export default App;
+ 
