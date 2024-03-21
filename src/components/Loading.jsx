2a265b78f80ee2f16d7a46a6b306ebc5991073f9
okari <nyandika15@gmail.com>
2024-03-21 11:41:10 +0300

    Create Loading.jsx

---
+++ src/components/Loading.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import '../Styles/LoadingScreen.css';
+
+const LoadingScreen = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className={`loading-screen ${loading ? 'active' : ''}`}>
+      <div className="loading-animation">
+        {/* Your loading animation goes here */}
+      </div>
+      <div className="loading-text">
+        Loading... <br />
+        Okari Nyandika
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
