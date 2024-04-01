35810d83ddf0d30a0207fe2d8ac5198080f58473
okari <nyandika15@gmail.com>
2024-04-01 14:09:45 +0300

    Update Introduction.jsx

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,36 +1,65 @@
 import React from 'react';
+import {
+  IoLogoInstagram,
+  IoLogoTwitter,
+  IoLogoWhatsapp,
+} from 'react-icons/io5';
+import EmailIcon from '@mui/icons-material/Email';
+import '../Styles/IntroductionScreen.css';
 
-const Introduction = () => {
-  return (
-    <div>
-      <div>
-        <h2>I'm Okari Nyandika. Glad to see you!</h2>
-        <p>
-          I'm a software developer. I can help you build a mobile app, website
-          or product. Look through some of my work and experience. If you like
-          what you see and have a project you need coded, don't hesitate to
-          contact me.
-        </p>
-        <div>
-          <p>LET'S CONNECT</p>
-          <ul>
-            <li>
-              <a href="/"></a>
-            </li>
-            <li>
-              <a href="/"></a>
-            </li>
-            <li>
-              <a href="/"></a>
-            </li>
-            <li>
-              <a href="/"></a>
-            </li>
-          </ul>
-        </div>
+const Introduction = () => (
+  <div className="introductioncontainer">
+    <div className="introductioncontent">
+      <h2 className="introductiontitle">
+        I'm <span className="span"> Okari Nyandika </span>
+        <br />
+        Glad to see you!
+      </h2>
+      <p className="introductiontext">
+        I'm a{' '}
+        <span
+          style={{
+            fontWeight: 'bold',
+          }}
+        >
+          software developer
+        </span>
+        . I can help you build a mobile app, website or product. Look through
+        some of my work and experience. If you like what you see and have a
+        project you need coded, don't hesitate to contact me.
+      </p>
+      <div className="connectsection">
+        <p className="connecttext">LET'S CONNECT</p>
+        <ul className="sociallinks">
+          <li className="linked">
+            <a href="/" className="sociallink">
+              <IoLogoInstagram color="#505F79" size={24} />
+            </a>
+          </li>
+          <li className="linked">
+            <a href="/" className="sociallink">
+              <IoLogoTwitter color="#505F79" size={24} />
+            </a>
+          </li>
+          <li className="linked">
+            <a href="/" className="sociallink">
+              <EmailIcon
+                size={24}
+                style={{
+                  color: '#505F79',
+                }}
+              />
+            </a>
+          </li>
+          <li className="linked">
+            <a href="/" className="sociallink">
+              <IoLogoWhatsapp color="#505F79" size={24} />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Introduction;
