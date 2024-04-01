4fcfafa05cbbcb0f6688bf2161297f69be26084e
okari <nyandika15@gmail.com>
2024-04-01 14:09:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
-import Navigation from './components/Navigation';
 import About from './components/About';
 import Introduction from './components/Introduction';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Navigation />} />
+          <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
@@ -24,3 +24,5 @@ function App() {
 }
 
 export default App;
+
+// Add a min height to all pages
