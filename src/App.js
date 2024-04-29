bd1a890e60554786b3b81e1ff9f3cfd107f7b96a
okari <nyandika15@gmail.com>
2024-04-29 09:22:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Home from './components/Home';
-import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import About from './components/About';
 import Introduction from './components/Introduction';
 import Home from './components/Home';
+import Page from './screens/Page';
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/portfolio" element={<Page />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
           <Route path="/intro" element={<Introduction />} />
