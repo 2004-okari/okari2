81bd153633d4209d1eb131b759e68d0a62233969
okari <nyandika15@gmail.com>
2024-04-29 09:22:19 +0300

    Create Page.jsx

---
+++ src/screens/Page.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Portfolio from '../components/Portfolio';
+
+const Page = () => (
+  <div
+    style={{
+      padding: 0,
+      margin: 0,
+      backgroundColor: '#f5f7f8',
+    }}
+  >
+    <h1
+      style={{
+        fontFamily: 'Simonetta',
+        fontSize: '62px',
+        fontWeight: 'bold',
+        color: '#172b4d',
+        paddingTop: '42px',
+        margin: '0 0 -14px 0',
+        textAlign: 'center',
+      }}
+    >
+      Portfolio
+      <sup
+        style={{
+          fontSize: '12px',
+          fontWeight: 'normal',
+          color: '#344563',
+        }}
+      >
+        Okari ron
+      </sup>
+    </h1>
+    <div>
+      <Portfolio />
+    </div>
+  </div>
+);
+
+export default Page;
