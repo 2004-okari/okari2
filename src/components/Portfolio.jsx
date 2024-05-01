bc8e527cf2cc7aaf3c9750d60265bd0f394e1e0e
okari <nyandika15@gmail.com>
2024-05-01 14:32:48 +0300

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -38,7 +38,7 @@ const Portfolio = () => {
       description:
         'Math Magicians is not just your ordinary calculator app; it&apos;s a versatile and multi-functional tool that simplifies your everyday calculations and offers a dose of inspiration along the way. With multiple pages, including Home, Calculator, and Quote Generator, Math Magicians transforms your math-solving routine into an uplifting and productive experience.',
       tags: ['react', 'react-router', 'APIs', 'redux'],
-      link: 'https://magician-math.netlify.app/',
+      link: 'https://math-magicians-git-dev-2004okaris-projects.vercel.app',
     },
     {
       id: 2,
@@ -49,7 +49,7 @@ const Portfolio = () => {
       description:
         'Embark on an interstellar journey with SpaceXplorer, a cutting-edge React application that lets you explore the cosmos like never before. SpaceXplorer is your ultimate portal for discovering spaceships, joining space missions, and booking seats for the adventure of a lifetime. Whether you&apos;re a space enthusiast, aspiring astronaut, or curious traveller, SpaceXplorer turns your dreams of space exploration into reality.',
       tags: ['react', 'redux', 'APIs', 'bootstrap'],
-      link: 'https://sprightly-gaufre-7108e7.netlify.app/',
+      link: 'https://react-capstone-jjmhoo8fd-2004okaris-projects.vercel.app',
     },
     {
       id: 3,
@@ -60,7 +60,7 @@ const Portfolio = () => {
       description:
         'Welcome to TipPal, your ultimate tip calculator for every occasion. Whether you’re dining out, splitting the bill, or tipping for exceptional service, TipPal makes tipping a breeze. With its intuitive design, user-friendly interface, and customizable features, TipPal ensures that you tip right every time.',
       tags: ['Front-end development', 'Responsive Design'],
-      link: '',
+      link: 'https://tip-calculator-l632.vercel.app/',
     },
     {
       id: 4,
@@ -71,7 +71,7 @@ const Portfolio = () => {
       description:
         'Introducing EasyTask, your go-to web app for simplifying your daily tasks and boosting productivity. EasyTask is a straightforward and user-friendly to-do list application designed to help you stay organized and on top of your goals, whether it&apos;s managing work assignments, personal chores, or planning projects.',
       tags: ['HTML', 'CSS', 'JavaScript', 'Responsive Design'],
-      link: '',
+      link: 'https://easytask-five.vercel.app/',
     },
     {
       id: 5,
@@ -82,7 +82,7 @@ const Portfolio = () => {
       description:
         'Introducing Leaderboard, your go-to leaderboard for storing and tracking player scores in a variety of games and competitions. Whether you&apos;re hosting a gaming tournament, managing sports leagues, or simply tracking scores for friendly competitions, Leaderboard makes scorekeeping effortless and fun. With Leaderboard&apos;s intuitive design and user-friendly features, you can keep tabs on scores and rankings with ease.',
       tags: ['HTML', 'CSS', 'JavaScript', 'local storage'],
-      link: '',
+      link: 'https://leaderboard-fawn.vercel.app/',
     },
     {
       id: 6,
@@ -93,7 +93,7 @@ const Portfolio = () => {
       description:
         'Welcome to the DevSummit Hub, your all-in-one landing page designed exclusively for the Developer&apos;s Summit! Whether you&apos;re a seasoned developer, a tech enthusiast, or a first-time attendee, DevSummit Hub is your digital gateway to a world of learning, networking, and innovation.',
       tags: ['HTML', 'CSS', 'JavaScript', 'Responsive design'],
-      link: '',
+      link: 'https://2004-okari.github.io/CAPSTONE-01/',
     },
     {
       id: 7,
@@ -104,18 +104,7 @@ const Portfolio = () => {
       description:
         'Welcome to the online portfolio showcase of X, where creativity, innovation, and expertise converge. This dynamic website is your gateway to a comprehensive display of X&apos;s professional journey, achievements, and creative works. Whether you&apos;re a potential employer, client, or simply an admirer of exceptional talent, this portfolio website offers a glimpse into the world of X.',
       tags: ['Front-end development', 'Responsive Design'],
-      link: '',
-    },
-    {
-      id: 8,
-      title: 'SportySB',
-      category: 'Web App Development',
-      year: '2023',
-      image: footbal,
-      description:
-        'Introducing SportySB Stats Tracker, the ultimate web app for sports enthusiasts, coaches, and teams. SportySB Stats Tracker harnesses the power of the SportySB API to provide real-time sports statistics and insights, making it easier than ever to analyze and improve performance. Whether you&apos;re a coach seeking data-driven strategies or a sports fan tracking your favorite team, SportySB Stats Tracker is your all-in-one sports data companion.',
-      tags: ['react', 'redux', 'APIs', 'Responsive Design'],
-      link: '',
+      link: 'https://2004-okari.github.io/MY-PORTFOLIO/',
     },
   ];
 
