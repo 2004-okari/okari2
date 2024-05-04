4c37f5219d0d2e3ff6098d05f969d5e311a6aef8
okari <nyandika15@gmail.com>
2024-05-04 12:58:07 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Introduction from './Introduction';
-import Portfolio from './Portfolio';
 import About from './About';
 import Navigation from './Navigation';
 import Contact from './Contact';
+import Page from '../screens/Page';
 
 const Home = () => (
   <>
     <Navigation />
     <Introduction />
-    <Portfolio />
+    <Page />
     <About />
     <Contact />
   </>
